855b49a0f90b35e2cdc1b536b97d92cc3bdfd057
costa0077 <lilfelipecosta@gmail.com>
2024-11-23 21:33:36 -0300

    wip

--- ecommerce-backend/routes/orders.js
+++
@@ -1,7 +0,0 @@
-module.exports = (sequelize, DataTypes) => {
-    const Order = sequelize.define('Order', {
-        status: { type: DataTypes.STRING, defaultValue: 'Aguardando Pagamento' },
-    });
-
-    return Order;
-};
